267af6a371392792c54ab31104ebf7cfedf8fe8c
Viktor <kaszas.viktor1993@gmail.com>
2019-07-14 23:41:14 +0200

    last bug fix from feedback

--- src/main/java/calculation/WinnerCalculator.java
+++ src/main/java/calculation/WinnerCalculator.java
@@ -1,14 +1,11 @@
 package calculation;
 
 import validation.FileValidation;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.*;
 
 public class WinnerCalculator {
 
-    private int[] lotteryRandomNumbers;
+    private List<Integer> lotteryRandomNumbers;
     private HashMap<Integer, Integer> winners;
 
     private static final String NUMBER_MATCHING_TEXT = "Numbers matching";
@@ -34,108 +31,18 @@ public class WinnerCalculator {
         winners.put(4, 0);
         winners.put(5, 0);
         fileValidation.checkInputFile(inputFilePath);
-        lotteryRandomNumbers = new NumberGenerator().generateLotteryNumbers();
+        lotteryRandomNumbers = new ArrayList<>();
+        lotteryRandomNumbers = Arrays.asList(new NumberGenerator().readLotteryNumbersFromConsole());
     }
-
-//    /**
-//        A beadott fájl vizsgálálása.
-//        Előszört a fájl hosszát méri le és ha nem nagyobb mint 10 millió akkor elkezdődik a sorok vizsgálása.
-//     */
-//    private void checkInputFile() {
-//        System.out.println(CHECKING_TEXT);
-//        try {
-//            Files.readAllLines(fileValidation.getFilePath(inputFilePath), StandardCharsets.US_ASCII)
-//                    .forEach(line -> fileValidation.checkLine(line));
-//            if (fileValidation.getCountX() > MAX_LINES) {
-//                throw new IOException(FILE_EXCEPTION_MESSAGE);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.exit(-1);
-//        }
-//        System.out.println(READY_TEXT);
-//    }
-
-//    /**
-//     * Vissza adja Beadott file elérési útját Path objektumként
-//     * @param inputFilePath
-//     * @return
-//     * @throws IOException
-//     */
-//    private Path getFilePath(String inputFilePath) throws IOException {
-//        try {
-//            return Files.list(Paths.get("."))
-//                    .filter(Files::isRegularFile)
-//                    .filter(filePath -> inputFilePath.equals(filePath.getFileName().toString()))
-//                    .collect(Collectors.toList()).get(0);
-//        } catch (IndexOutOfBoundsException ex){
-//            throw new IndexOutOfBoundsException(BAD_INPUT_FILE_PATH_TEXT);
-//        }
-//    }
-
-//    /**
-//     * A sort megn&eacute;zi, hogy a Matcherben a felt&eacute;teleknek megfelel majd levizsg&aacute;lja, hogy nincs benne 90-es sz&aacute;mn&aacute;l nagyobb
-//     * vagy 0 vagy ann&aacute;l kissebb.
-//     * Illetve nem csak 4 sz&aacute;m van megadva.
-//     * vagy 1-es sz&aacute;mn&aacute;l kissebb.
-//     * @param line
-//     * @return boolean
-//     */
-//    private boolean checkLine(String line) {
-//        countX += 1;
-//        if(TAG_REGEX.matcher(line).matches()
-//                && line.split("\\D+").length == 5
-//                && numberBetweenCheck(line.split("\\D+"))){
-//            return true;
-//        } else {
-//            badLines.add(line);
-//            return false;
-//        }
-//    }
-
-//    /**
-//     * Számok megnézése hogy 1 és 90 között van.
-//     * @param lineRegexArray
-//     * @return
-//     */
-//    private boolean numberBetweenCheck(String[] lineRegexArray) {
-//        return Arrays.stream(lineRegexArray).noneMatch(numbers -> Integer.valueOf(numbers) < MIN_NUMBER)
-//                &&
-//                Arrays.stream(lineRegexArray).noneMatch(numbers -> Integer.valueOf(numbers) > MAX_NUMBER);
-//    }
-
-//    /**
-//     * Az 5 lottószám generálása 1-től 90-ig.
-//     * Ha egy generált szám már létezik a tömben akkor generál új számot.
-//     */
-//    public void generateLotteryNumbers() {
-//        System.out.println(LOTTERRY_NUMBERS_TEXT);
-////        lotteryRandomNumbers = ThreadLocalRandom.current().ints(1, 90).distinct().limit(5).toArray();
-//        lotteryRandomNumbers = new int[5];
-//        lotteryRandomNumbers[0] = 33;
-//        lotteryRandomNumbers[1] = 73;
-//        lotteryRandomNumbers[2] = 17;
-//        lotteryRandomNumbers[3] = 67;
-//        lotteryRandomNumbers[4] = 30;
-//        Arrays.stream(lotteryRandomNumbers).forEach(System.out::println);
-//    }
 
     /**
      * A nyertesek összeszámálása a generált számok alapján illetve a kész kalkuláció megjelenítése konzolon.
      */
     public void startCalculation() {
-        try {
-            Files.readAllLines(fileValidation.getFilePath(inputFilePath), StandardCharsets.US_ASCII)
-                    .stream()
-                    .filter(line -> !badLines.contains(line))
-                    .forEach(this::countWinnersByInputData);
+            fileValidation.getLinesToArrayList().stream().filter(integers -> !badLines.contains(integers))
+                    .forEach(integers -> countWinnersByInputData(integers));
             showCalculatedWinners();
             System.out.println(BAD_DATA_LINE_EXCEPTION + badLines.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
     }
 
     /**
@@ -143,8 +50,9 @@ public class WinnerCalculator {
      *
      * @param line
      */
-    private void countWinnersByInputData(String line) {
-        hitsNumbers = Arrays.stream(lotteryRandomNumbers).filter(value -> line.contains(String.valueOf(value))).count();
+    private ArrayList<Integer> lineToIntList = new ArrayList<>();
+    private void countWinnersByInputData(ArrayList<Integer> line) {
+        hitsNumbers = lotteryRandomNumbers.stream().filter(value -> line.contains(value)).count();
         if (hitsNumbers > 1) {
             if (hitsNumbers == 5) {
                 System.out.println(WINNER_DATA_LINE_TEXT + line);
